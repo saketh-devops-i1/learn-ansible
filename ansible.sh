ecd197c77015098b67bfe33c3e944555365d5837
saketh <uday.kundoor123@gmail.com>
2024-04-13 22:25:23 +0530

    password updated

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
 
-ansible-playbook -i $component-$env.devops-saketh.online, -e ansible_user=ec2-user -e ansible_password=DevOps321, -e env=$env -e role_name=$component expense.yml
+ansible-playbook -i $component-$env.devops-saketh.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
