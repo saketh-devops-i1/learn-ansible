b2f67fd791dc358103579d8df406dfd662177b05
saketh <uday.kundoor123@gmail.com>
2024-04-19 16:59:59 +0530

    vault token

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,4 @@
 component=$1
 
+ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
 ansible-playbook -i $component-$env.devops-saketh.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
